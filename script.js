61d927e71c85de0fb11d0eeaf82f831f39e808ba
Eli Dawson <edawson@alumni.berklee.edu>
2023-03-28 21:56:42 -0400

    added nav, basic page layout, created html for pages

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+// document.querySelector("body").appendChild(h2);
